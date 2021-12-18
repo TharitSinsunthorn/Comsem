c6101b96d2c5bc5f85ae7b88650d21c7e8022e36
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2021-12-18 13:45:13 +0700

    pb23

---
+++ Problem2-3.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+font = pygame.font.Font(None, 50)
+text_img = font.render("Hello, pygame", True, pygame.Color("green"))
+
+# For image mouse
+img_org = pygame.image.load("../../assets/player/p1_walk01.png")
+player_img = img_org.convert()
+Pwidth = player_img.get_width()
+Pheight = player_img.get_height()
+
+while True:
+    pygame.event.clear()
+    key_pressed = pygame.key.get_pressed()
+    if key_pressed[pygame.K_ESCAPE]:
+        break
+    mouse_pos = pygame.mouse.get_pos()
+    
+    screen.fill(pygame.Color("black"))
+    #method to paste image to the mouse position
+    mouse_x, mouse_y = mouse_pos
+    
+    screen.blit(player_img, [mouse_x - Pwidth/2, mouse_y - Pheight/2])
+    screen.blit(text_img, (mouse_x + 100 - Pwidth/2, mouse_y - Pheight/2))
+    # For red circle
+    # pygame.draw.circle(screen, pygame.Color("red"), pygame.mouse.get_pos(), 30)
+    pygame.display.update()
+    
+pygame.quit()
