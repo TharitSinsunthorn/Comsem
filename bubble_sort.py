933b8193b62a0f4c9abd573792c43def330c3b0e
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2022-01-20 01:56:05 +0700

    chap6_bubblekey

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,17 @@
-def bubble_sort(x, compare_func=None):
-    if compare_func is None:
+def bubble_sort(x, key=None, reverse=False):
+    if key is None:
+        def key(x):
+            return x
+
+    if reverse:
+        def compare_func(a, b):
+            return a < b
+    else:
         def compare_func(a, b):
             return a > b
     
     n = len(x)
     for k in range(n-1, 0, -1):
         for i in range(k):
-            if compare_func(x[i], x[i+1]):
+            if compare_func(key(x[i]), key(x[i+1])):
                 x[i], x[i+1] = x[i+1], x[i]
