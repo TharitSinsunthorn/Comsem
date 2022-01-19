8d09336a50ccb78f4a98890e0767442e2c55abc5
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2022-01-20 01:39:02 +0700

    chap6_bubblemark

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,5 +2,5 @@ def bubble_sort(x):
     n = len(x)
     for k in range(n-1, 0, -1):
         for i in range(k):
-            if x[i] > x[i+1]:
+            if x[i].math_mark > x[i+1].math_mark:
                 x[i], x[i+1] = x[i+1], x[i]
