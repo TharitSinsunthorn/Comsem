5d5b69f5462662dc4dd4b1fd6d123413f55460b6
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2022-01-20 01:41:32 +0700

    chap6_bubble_comparefunc

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,10 @@
-def bubble_sort(x):
+def bubble_sort(x, compare_func=None):
+    if compare_func is None:
+        def compare_func(a, b):
+            return a > b
+    
     n = len(x)
     for k in range(n-1, 0, -1):
         for i in range(k):
-            if x[i].math_mark > x[i+1].math_mark:
+            if compare_func(x[i], x[i+1]):
                 x[i], x[i+1] = x[i+1], x[i]
