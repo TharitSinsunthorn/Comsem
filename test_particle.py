ea09fc4bf0c9472db10b94be984b68a8377b210f
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2022-01-19 23:16:13 +0700

    test_particle_isalive

--- test_particle.py
+++ test_particle.py
@@ -1,20 +1,19 @@
 import pytest
 import particle
 
-@pytest.mark.parametrize("x0, y0, vx0, vy0", [
-    (0, 0, 5, 10),
-    (300, 200, -7, 12),
-    (500, 300, -4, -10),
+@pytest.mark.parametrize("x0, y0, vx0, vy0, expected", [
+    (300, 200, 5, 10, True),
+    (300, 200, -5, -10, True),
+    (599, 200, 5, 10, False),
+    (300, 399, 5, 10, False),
+    (-1, 399, 5, 10, False),
+    (0, 0, 5, 10, True),
+    (0, 0, -5, 10, False),
 ])
-def test_particle_move(x0, y0, vx0, vy0):
+def test_particle_liveness_after_move(x0, y0, vx0, vy0, expected):
     width, height = 600, 400
-    dt, g = 2, 4
+    dt, g = 1, 0.5
     w = particle.World(width, height, dt, g)
-
     p = particle.Particle((x0, y0), (vx0, vy0), w)
     p.update()
-    assert p.x == x0 + vx0 * dt
-    assert p.y == y0 + (vy0 + g * dt) * dt
-    p.update()
-    assert p.x == x0 + 2 * vx0 * dt
-    assert p.y == y0 + (vy0 + g * dt) * dt + (vy0 + 2 * g * dt) * dt
+    assert p.is_alive == expected
