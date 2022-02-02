055c5543376c15c89ca0ffe31e71923870a72ab0
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2022-02-03 02:39:31 +0700

    nothing

--- testgit.py
+++ testgit.py
@@ -1,72 +1,19 @@
-import pygame
-import random
+import pytest
+import particle
 
-class Particle:
-    pass
-
-def init_particle(p, pos, vel):
-    p.is_alive = True
-    p.x, p.y = pos
-    p.vx, p.vy = vel
-    
-def update_particle(p, width, height, dt, gy):
-    p.vy += gy * dt
-    p.x += p.vx * dt
-    p.y += p.vy * dt
-    if p.x < 0 or p.x > width or p.y > height:
-        p.is_alive = False
-        
-def draw_particle(p, screen):
-    radius = 10
-    pygame.draw.circle(screen, pygame.Color("green"), (p.x, p.y), radius)
-    
-def main():
-    pygame.init()
+@pytest.mark.parametrize("x0, y0, vx0, vy0, expected", [
+    (300, 200, 5, 10, True),
+    (300, 200, -5, -10, True),
+    (599, 200, 5, 10, False),
+    (300, 399, 5, 10, False),
+    (-1, 399, 5, 10, False),
+    (0, 0, 5, 10, True),
+    (0, 0, -5, 10, False),
+])
+def test_particle_liveness_after_move(x0, y0, vx0, vy0, expected):
     width, height = 600, 400
-    screen = pygame.display.set_mode((width, height))
-    clock = pygame.time.Clock()
-    
-    dt = 1.0
-    gy = 0.5
-    particle_list = []
-    p = Particle()
-    p.is_alive_ =  False
-    p.x, p.y = 0, 0
-    p.vx, p.vy = 0, 0
-    particle_is_alive = False
-    
-    while True:
-        frames_per_second = 60
-        clock.tick(frames_per_second)
-        
-        should_quit = False
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                should_quit = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    should_quit = True
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    p = Particle()
-                    vx = random.uniform(-10, 10)
-                    vy = random.uniform(-10, 0)
-                    init_particle(p, event.pos, (vx, vy))
-                    particle_list.append(p)
-        if should_quit:
-            break
-        
-        for p in particle_list:
-            update_particle(p, width, height, dt, gy)
-        
-        particle_list[:] = {p for p in particle_list if p.is_alive}
-        
-        screen.fill(pygame.Color("black"))
-        for p in particle_list:
-            draw_particle(p, screen)
-        pygame.display.update()
-        
-    pygame.quit()
-    
-if __name__ == "__main__":
-    main()
+    dt, g = 1, 0.5
+    w = particle.World(width, height, dt, g)
+    p = particle.Particle((x0, y0), (vx0, vy0), w)
+    p.update()
+    assert p.is_alive == expected
